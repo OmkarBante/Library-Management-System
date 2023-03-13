77392e19b308bbc0b444a5f05d6e53de5225df03
Omkar <omkarbante16@gmail.com>
2023-03-13 22:54:06 +0530

    first commit

---
+++ client/src/components/Button.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Button({
+  title,
+  variant = "contained",
+  color = "primary",
+  type = "button",
+  onClick,
+  fullWidth = false
+ 
+}) {
+  let className = fullWidth ? "w-100 rounded " : "pr-2 pl-2 rounded ";
+  if (variant === "contained") {
+    className += "bg-" + color + " text-white";
+  } else if (variant === "outlined" ) {
+    className += "border-" + color + " text-" + color;
+  }
+
+//   if (disabled) {
+//     className += "disabled-btn";
+//   }
+
+  return (
+    <button className={className} type={type} onClick={onClick}>
+      {title}
+    </button>
+  );
+}
+
+export default Button;
+
+
+
+
+
