5a8f9ee84dcd2cd7b906697048c0c81fe083cad7
OmkarBante <83286193+OmkarBante@users.noreply.github.com>
2023-03-14 19:54:28 +0530

    Update index.js

--- client/src/pages/Profile/BasicDetails/index.js
+++ client/src/pages/Profile/BasicDetails/index.js
@@ -4,6 +4,39 @@ import { useSelector } from "react-redux";
 
 function BasicDetails() {
   const { user } = useSelector((state) => state.users);
+  if(user.role === "patron"){
+    return (
+      <div>
+        <div className="rounded bg-secondary text-white flex flex-col p-2 w-50">
+          <div className="flex justify-between">
+            <h1 className="text-md">Name</h1>
+            <h1 className="text-md">{user.name}</h1>
+          </div>
+          <div className="flex justify-between">
+            <h1 className="text-md">Email</h1>
+            <h1 className="text-md">{user.email}</h1>
+          </div>
+          <div className="flex justify-between">
+            <h1 className="text-md">Phone</h1>
+            <h1 className="text-md">{user.phone}</h1>
+          </div>
+          <div className="flex justify-between">
+          
+            <h1 className="text-md">Role</h1>
+            <h1 className="text-md uppercase">student</h1>
+          </div>
+  
+          <div className="flex justify-between">
+            <h1 className="text-md">Registered On</h1>
+            <h1 className="text-md">{
+              moment(user.createdAt).format("MMM Do YYYY, h:mm a")
+            }</h1>
+          </div>
+        </div>
+      </div>
+    );
+          }
+  else{
   return (
     <div>
       <div className="rounded bg-secondary text-white flex flex-col p-2 w-50">
@@ -20,6 +53,7 @@ function BasicDetails() {
           <h1 className="text-md">{user.phone}</h1>
         </div>
         <div className="flex justify-between">
+        
           <h1 className="text-md">Role</h1>
           <h1 className="text-md uppercase">{user.role}</h1>
         </div>
@@ -33,6 +67,7 @@ function BasicDetails() {
       </div>
     </div>
   );
+        }
 }
 
 export default BasicDetails;
